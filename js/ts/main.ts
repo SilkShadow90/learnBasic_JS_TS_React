3b148db450fc27248851e1d2e9fc55e6d71cfd9e
AVEL90 <AVEL90@mail.ru>
2021-04-15 10:23:13 +0400

    z

--- js/ts/main.ts
+++ js/ts/main.ts
@@ -65,84 +65,94 @@
 // // Числа и оператор разделены ровно одним пробелом.
 // // Не лишним будет добавить обработку ошибок.
 
-interface AbstractFactory {
- createProductA(): AbstractProductA;
- createProductB(): AbstractProductB;
+// interface AbstractFactory {
+//  createProductA(): AbstractProductA;
+//  createProductB(): AbstractProductB;
+// }
+
+// class ConcreteFactory1 implements AbstractFactory {
+//  public createProductA(): AbstractProductA {
+//    return new ConcreteProductA1();
+//  }
+//  public createProductB(): AbstractProductB {
+//    return new ConcreteProductB1();
+//  }
+// }
+
+// class ConcreteFactory2 implements AbstractFactory {
+//  public createProductA(): AbstractProductA {
+//    return new ConcreteProductA2();
+//  }
+//  public createProductB(): AbstractProductB {
+//    return new ConcreteProductB2();
+//  }
+// }
+
+// interface AbstractProductA {
+//  usefulFunctionA(): string;
+// }
+
+// class ConcreteProductA1 implements AbstractProductA {
+//  public usefulFunctionA(): string {
+//    return 'The result of the product A1.';
+//  }
+// }
+
+// class ConcreteProductA2 implements AbstractProductA {
+//  public usefulFunctionA(): string {
+//    return 'The result of the product A2.';
+//  }
+// }
+
+// interface AbstractProductB {
+//  usefulFunctionB(): string;
+//  anotherUsefulFunctionB(collaborator: AbstractProductA): string;
+// }
+
+// class ConcreteProductB1 implements AbstractProductB {
+//  public usefulFunctionB(): string {
+//    return 'The result of the product B1.';
+//  }
+
+//  public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+//    const result = collaborator.usefulFunctionA();
+//    return The result of the B1 collaborating with the (${result});
+//  }
+// }
+
+// class ConcreteProductB2 implements AbstractProductB {
+//  public usefulFunctionB(): string {
+//    return 'The result of the product B2.';
+//  }
+
+//  public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+//    const result = collaborator.usefulFunctionA();
+//    return The result of the B2 collaborating with the (${result});
+//  }
+// }
+
+// function clientCode(factory: AbstractFactory) {
+//  const productA = factory.createProductA();
+//  const productB = factory.createProductB();
+
+//  console.log(productB.usefulFunctionB());
+//  console.log(productB.anotherUsefulFunctionB(productA));
+// }
+
+// console.log('Client: Testing client code with the first factory type...');
+// clientCode(new ConcreteFactory1());
+
+// console.log('');
+
+// console.log('Client: Testing the same client code with the second factory type...');
+// clientCode(new ConcreteFactory2())
+
+// Напишите функцию, которая принимает строку из чисел и возвращает строку с максимальным и минимальным числами из начальной строки
+
+highAndLow(str){
+  
 }
-
-class ConcreteFactory1 implements AbstractFactory {
- public createProductA(): AbstractProductA {
-   return new ConcreteProductA1();
- }
- public createProductB(): AbstractProductB {
-   return new ConcreteProductB1();
- }
-}
-
-class ConcreteFactory2 implements AbstractFactory {
- public createProductA(): AbstractProductA {
-   return new ConcreteProductA2();
- }
- public createProductB(): AbstractProductB {
-   return new ConcreteProductB2();
- }
-}
-
-interface AbstractProductA {
- usefulFunctionA(): string;
-}
-
-class ConcreteProductA1 implements AbstractProductA {
- public usefulFunctionA(): string {
-   return 'The result of the product A1.';
- }
-}
-
-class ConcreteProductA2 implements AbstractProductA {
- public usefulFunctionA(): string {
-   return 'The result of the product A2.';
- }
-}
-
-interface AbstractProductB {
- usefulFunctionB(): string;
- anotherUsefulFunctionB(collaborator: AbstractProductA): string;
-}
-
-class ConcreteProductB1 implements AbstractProductB {
- public usefulFunctionB(): string {
-   return 'The result of the product B1.';
- }
-
- public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
-   const result = collaborator.usefulFunctionA();
-   return The result of the B1 collaborating with the (${result});
- }
-}
-
-class ConcreteProductB2 implements AbstractProductB {
- public usefulFunctionB(): string {
-   return 'The result of the product B2.';
- }
-
- public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
-   const result = collaborator.usefulFunctionA();
-   return The result of the B2 collaborating with the (${result});
- }
-}
-
-function clientCode(factory: AbstractFactory) {
- const productA = factory.createProductA();
- const productB = factory.createProductB();
-
- console.log(productB.usefulFunctionB());
- console.log(productB.anotherUsefulFunctionB(productA));
-}
-
-console.log('Client: Testing client code with the first factory type...');
-clientCode(new ConcreteFactory1());
-
-console.log('');
-
-console.log('Client: Testing the same client code with the second factory type...');
-clientCode(new ConcreteFactory2())
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6");
