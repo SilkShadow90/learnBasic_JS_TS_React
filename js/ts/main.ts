9dc407d23309781a06d91315199748c418d9dcb6
akeros <mixali4kin@bk.ru>
2021-05-23 22:16:47 +0400

    в

--- js/ts/main.ts
+++ js/ts/main.ts
@@ -122,3 +122,223 @@
 // // Для этой задачи не нужны скобки или сложные выражения.
 // // Числа и оператор разделены ровно одним пробелом.
 // // Не лишним будет добавить обработку ошибок.
+
+// Трансформировать в массив имён
+// важность: 5
+// У вас есть массив объектов user, и в каждом из них есть user.name.
+//  Напишите код, который преобразует их в массив имён.
+
+// Например:
+//
+// const vasya = { name: 'Вася', age: 25 };
+// const petya = { name: 'Петя', age: 30 };
+// const masha = { name: 'Маша', age: 28 };
+
+// const users = [vasya, petya, masha];
+
+// const names = users.map((item) => item.name);
+
+// console.log(names); // Вася, Петя, Маша
+
+//
+// важность: 5
+// У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
+
+// Напишите код, который создаст ещё один массив объектов с параметрами
+//  id и fullName, где fullName – состоит из name и surname.
+
+// Например:
+// interface IUser {
+//     name: string;
+//     surname: string;
+//     id: number;
+// }
+// const vasya: IUser = { name: "Вася", surname: "Пупкин", id: 1 };
+// const petya: IUser = { name: "Петя", surname: "Иванов", id: 2 };
+// const masha: IUser = { name: "Маша", surname: "Петрова", id: 3 };
+
+// const users: IUser[] = [vasya, petya, masha];
+
+// const usersMapped = users.map((user) => ({
+//   fullName: `${user.name} ${user.surname}`,
+//   id: user.id,
+// }));
+
+// /*
+// usersMapped = [
+//   { fullName: "Вася Пупкин", id: 1 },
+//   { fullName: "Петя Иванов", id: 2 },
+//   { fullName: "Маша Петрова", id: 3 }
+// ]
+// */
+
+// console.log(usersMapped[1].id)// 1
+// console.log(usersMapped[1].fullName)// Вася Пупкин
+
+// // Итак, на самом деле вам нужно трансформировать один массив объектов в другой.
+//  Попробуйте использовать =>. Это небольшая уловка.
+// Отсортировать пользователей по возрасту
+// важность: 5
+
+// Напишите функцию sortByAge(users),
+//  которая принимает массив объектов со свойством age и сортирует их по нему.
+
+// Например:
+// interface IUser {
+//   name: string;
+//   age: number;
+// }
+// const vasya: IUser = { name: "Вася", age: 25 };
+// const petya: IUser = { name: "Петя", age: 30 };
+// const masha: IUser = { name: "Маша", age: 28 };
+
+// function sortByAge(users: IUser[]): void {
+//   users.sort((a, b) => a.age - b.age);
+// }
+// const arr: IUser[] = [vasya, petya, masha];
+
+// sortByAge(arr);
+
+// // теперь: [vasya, masha, petya]
+// console.log(arr[0].name); // Вася
+// console.log(arr[1].name); // Маша
+// console.log(arr[2].name); // Петя
+// Получить средний возраст
+// важность: 4
+// Напишите функцию getAverageAge(users),
+//  которая принимает массив объектов со свойством age и возвращает средний возраст.
+
+// Формула вычисления среднего арифметического значения: (age1 + age2 + ... + ageN) / N.
+
+// Например:
+// Например:
+// interface IUser {
+//   name: string;
+//   age: number;
+// }
+
+// function getAverageAge(users: IUser[]): number {
+//   return users.reduce((acc: number, user: IUser) => acc + user.age, 0) / users.length;
+// }
+
+// const vasya: IUser = { name: "Вася", age: 25 };
+// const petya: IUser = { name: "Петя", age: 30 };
+// const masha: IUser = { name: "Маша", age: 29 };
+
+// const arr: IUser[] = [vasya, petya, masha];
+
+// console.log(getAverageAge(arr)); // (25 + 30 + 29) / 3 = 28
+// Оставить уникальные элементы массива
+// важность: 4
+// Пусть arr – массив строк.
+
+// Напишите функцию unique(arr), которая возвращает массив,
+//  содержащий только уникальные элементы arr.
+
+// Например:
+
+// function unique(arr: string[]): string[] {
+//   return arr.reduce((acc: string[], item: string) => {
+//     if (!acc.includes(item)) {
+//       acc.push(item);
+//     }
+//     return acc;
+//   }, []);
+// }
+
+// let strings = ["кришна", "кришна", "харе", "харе",
+//   "харе", "харе", "кришна", "кришна", ":-O"
+// ];
+
+// console.log(unique(strings)); // кришна, харе, :-O
+// Ошибка создания экземпляра класса
+// важность: 5
+// В коде ниже класс Rabbit наследует Animal.
+
+// К сожалению, объект класса Rabbit не создаётся. Что не так? Исправьте ошибку.
+
+// class Animal {
+
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+// }
+
+// class Rabbit extends Animal {
+//   constructor(name) {
+//     super(name)
+//     this.created = Date.now();
+//   }
+// }
+
+// let rabbit = new Rabbit("Белый кролик"); // Error: this is not defined
+// console.log(rabbit.name);
+
+
+// class Clock {
+//   readonly template: string;
+
+//   constructor({ template }) {
+//     this.template = template;
+//   }
+
+//   timer: number;
+
+//   render() {
+//     const date = new Date();
+
+//     let hours = String(date.getHours());
+//     if (hours.length < 2) {
+//       hours = `0${hours}`;
+//     }
+
+//     let mins = String(date.getMinutes());
+//     if (mins.length < 2) {
+//       mins = `0${mins}`;
+//     }
+
+//     let secs = String(date.getSeconds());
+//     if (secs.length < 2) {
+//       secs = `0${secs}`;
+//     }
+
+//     const output = this.template
+//       .replace('h', hours)
+//       .replace('m', mins)
+//       .replace('s', secs);
+
+//     console.log(output);
+//   }
+
+//   stop() {
+//     clearInterval(this.timer);
+//   }
+
+//   start() {
+//     this.render();
+//     this.timer = setInterval(this.render.bind(this), 1000);
+//   }
+// }
+// class ExtendedClock extends Clock {
+//   readonly precision: number;
+
+//   constructor({ template, precision = 1000 }) {
+//     super({ template })
+//     this.precision = precision;
+//   }
+
+//   start() {
+//     this.render();
+//     this.timer = setInterval(this.render.bind(this), this.precision);
+//   }
+// }
+
+// const clock = new ExtendedClock({ template: 'h:m:s', precision: undefined });
+// clock.start();
+// Создайте новый класс ExtendedClock, который будет наследоваться от Clock и
+//  добавьте параметр precision – количество миллисекунд между «тиками».
+//  Установите значение в 1000 (1 секунда) по умолчанию.
+
+// Сохраните ваш код в файл extended-clock.js
+// Не изменяйте класс clock.js. Расширьте его.
