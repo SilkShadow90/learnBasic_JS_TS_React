a7b7163d75d25f49902a4c01231c2bd40008bef3
AVEL90 <AVEL90@mail.ru>
2021-05-26 13:25:19 +0400

    рабочий король и ладья

--- js/ts/Chess/Figures/King.ts
+++ js/ts/Chess/Figures/King.ts
@@ -4,7 +4,7 @@ import { THorizontal, TVertical } from '../interfaces/index';
 import kingWhite from '../../../../img/king_white.svg';
 // @ts-ignore
 import kingBlack from '../../../../img/king_black.svg';
-import { charList } from '../tools';
+import { charList, numList } from '../tools';
 
 export default class King extends Figure {
   figureName = 'King';
@@ -20,8 +20,8 @@ export default class King extends Figure {
       [charList[arraySearch + 1], currentPosition[1] - 1 as TVertical],
       [charList[arraySearch - 1], currentPosition[1] + 1 as TVertical],
       [charList[arraySearch - 1], currentPosition[1] - 1 as TVertical],
-      [charList[arraySearch - 1], currentPosition[1]],
       [charList[arraySearch + 1], currentPosition[1]],
+      [charList[arraySearch - 1], currentPosition[1]],
       [charList[arraySearch], currentPosition[1] + 1 as TVertical],
       [charList[arraySearch], currentPosition[1] - 1 as TVertical],
     ];
@@ -29,8 +29,8 @@ export default class King extends Figure {
     if (currentPosition === this.startPosition) {
       possiblePositions.push(castling[0], castling[1]);
     }
-
-    return possiblePositions;
+    console.log(possiblePositions);
+    return possiblePositions.filter(([char, num]) => char && numList.includes(num));
   };
 
   render = (): HTMLElement => this.prerender(kingWhite, kingBlack)
--- js/ts/Chess/Figures/Rook.ts
+++ js/ts/Chess/Figures/Rook.ts
@@ -9,12 +9,10 @@ import { charList, numList } from '../tools';
 export default class Rook extends Figure {
   figureName = 'Rook';
   getNextPositionMap = (): Array<[THorizontal, TVertical]> => {
-    const possiblePositions: Array<[THorizontal, TVertical]> = [];
     const currentPosition: [THorizontal, TVertical] = this.getPosition();
-    const horizontalMovement: [THorizontal, TVertical] = numList.filter((value) => value < numList.length + 1).map((value) => [currentPosition[0], value]);
+    const horizontalMovement = numList.filter((value) => value < numList.length + 1).map((value) => [currentPosition[0], value]);
     const verticalMovement = charList.filter((value) => value).map((value) => [value, currentPosition[1]]);
-    possiblePositions.push(horizontalMovement, verticalMovement);
-    console.log(possiblePositions);
+    const possiblePositions: Array<[THorizontal, TVertical]> = horizontalMovement.concat(verticalMovement);
 
     return possiblePositions;
   };
