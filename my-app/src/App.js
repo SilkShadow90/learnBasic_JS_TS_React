728e35bb43e3fdad9202b0233b0ff4ab2435fff0
AVEL90 <AVEL90@mail.ru>
2021-06-15 11:06:00 +0400

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
 }
-
-export default App;
+  
+const user = {
+    firstName: 'Марья',
+    lastName: 'Моревна'
+};
+  
+const element = (
+    <h1>
+      Здравствуй, {formatName(user)}!
+    </h1>
+);
+  
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
