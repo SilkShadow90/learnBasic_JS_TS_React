644af6607ada78195d6086ab4a371e3dd9505b71
AVEL90 <AVEL90@mail.ru>
2021-06-15 14:17:37 +0400

    a

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
-import ReactDOM from 'react-dom';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+function App() {
+    function formatName(user) {
+        return user.firstName + ' ' + user.lastName;
+      }
+      
+      const user = {
+        firstName: 'Марья',
+        lastName: 'Моревна'
+      };
+      
+     return (
+        <h1>
+          Здравствуй, {formatName(user)}!
+        </h1>
+      );
+      
 }
-  
-const user = {
-    firstName: 'Марья',
-    lastName: 'Моревна'
-};
-  
-const element = (
-    <h1>
-      Здравствуй, {formatName(user)}!
-    </h1>
-);
-  
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+
+export default App;
